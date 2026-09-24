3b84637a8a8991b1c5b26ce5ad8a264ebdbe21fe
K L Bhanu <klbm9999@gmail.com>
2026-09-24 22:25:36 +0530

    fix wezterm sizing issue, fix comment in keys.lua

--- home/.config/nvim/lua/keys.lua
+++ home/.config/nvim/lua/keys.lua
@@ -2,5 +2,5 @@
 vim.keymap.set('n', '<Esc>', ':w<CR>', { desc = 'Save' })
 -- select all
 vim.keymap.set('n','<C-a>', 'ggVG', { desc = 'Select All'})
--- pasting over a seletion no longer clobbers your clipboard
+-- pasting over a selection no longer clobbers your clipboard
 vim.cmd([[ xnoremap <expr> p 'pgv"'.v:register.'y' ]])
