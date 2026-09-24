4070fb5a85e8be785cf4f78ae6b68384ea825d46
K L Bhanu <klbm9999@gmail.com>
2026-09-24 15:55:42 +0530

    nvim updated with plugins

---
+++ home/.config/nvim/lua/vim_config.lua
@@ -0,0 +1,11 @@
+local o = vim.opt
+vim.g.mapleader = ' '		-- space is the leader key
+o.expandtab = true		-- spaces, not tabs
+o.shiftwidth = 2		-- 2 spaces per indent level
+o.number = true			-- absolute number on the cursor line, relative elsewhere
+o.relativenumber = true		-- relative line numbers for fast jumps
+o.ignorecase = true		-- search is case-insentitive by default
+o.smartcase = true		-- case-sensitive only if a capital is typed
+o.clipboard = 'unnamedplus'	-- share the system clipboard
+o.scrolloff = 16		-- keep cursor away from screenedge
+o.undofile = true		-- persistent undo across sessions
